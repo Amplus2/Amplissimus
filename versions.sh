ea577d00eace0702c09d5f3ae028a0e4bd85039d
chrissx <chrissx@chrissx.de>
2021-04-06 13:49:58 +0200

    tried doing releases from gh actions, which might work

--- versions.sh
+++ versions.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 shortversion='4.2'
 commitcount=$(git rev-list @ --count)
+echo "::set-output name=commit::$(git rev-parse @)"
 echo "::set-output name=short_version::$shortversion"
 echo "::set-output name=commit_count::$commitcount"
 echo "::set-output name=version::$shortversion.$(($commitcount - 1400))"
