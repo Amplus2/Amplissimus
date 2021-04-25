826a33dbc41b1b68d2333144401017a114df3636
chrissx <chrissx@chrissx.de>
2021-04-25 06:33:29 +0200

    changed the wpemails test to use our testpoint so we can test well

--- test/wpemails_test.dart
+++ test/wpemails_test.dart
@@ -4,12 +4,14 @@ import 'package:schttp/schttp.dart';
 import 'testlib.dart';
 
 void main() async {
-  final wpe = await wpemails('gympeg.de', ScHttpClient());
-  tests([testAssert(wpe.length > 50)], 'wpemails length');
+  final wpe = await wpemails('amplus.chrissx.de', ScHttpClient());
   tests(
-    wpe.values.map(
-      (e) => testAssert(RegExp('.+?\\..+?@gympeg\\.de').hasMatch(e)),
-    ),
+    [
+      testAssert(wpe.length >= 3),
+      testExpect(wpe['Häußler C.'], 'c.haeussler@amplus.chrissx.de'),
+      testExpect(wpe['Lehnert L.'], 'l.lehnert@amplus.chrissx.de'),
+      testExpect(wpe['Ganserer T.'], 't.ganserer@amplus.chrissx.de'),
+    ],
     'wpemails',
   );
 }
